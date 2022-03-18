e0d25cdb5354b7734931eee3fe542caf1e9818d4
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-03-18 13:33:46 -0300

    Creating and mounting routers

--- app.js
+++ app.js
@@ -152,21 +152,21 @@ const deleteUser = (req, res) => {
   });
 };
 
+//ROUTERS
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
 //ROUTES
 //tours routes
-app.route('/api/v1/tours').get(getAllTours).post(createTour);
-app
-  .route('/api/v1/tours/:id')
-  .get(getTour)
-  .patch(updateTour)
-  .delete(deleteTour);
+tourRouter.route('/').get(getAllTours).post(createTour);
+tourRouter.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 //users routes
-app.route('/api/v1/users').get(getAllUsers).post(createUser);
-app
-  .route('/api/v1/users/:id')
-  .get(getUser)
-  .patch(updateUser)
-  .delete(deleteUser);
+userRouter.route('/').get(getAllUsers).post(createUser);
+userRouter.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
+
+//mounting the routers
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', tourRouter);
 
 //establishing the port
 const port = 3000;
