61d3aa55d880df80ea1b52627c9e0527b5bac8ce
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-03-21 16:16:50 -0300

    Connecting to our MongoDB database with Mongoose

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 //dependencies
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 //reading the env variables from our config.env file and saving them into Node environment variables
 dotenv.config({ path: './config.env' });
+
+//first, creating the complete mongodb connection string, with the password for the database
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+//connecting to MongoDB database
+mongoose.connect(DB).then(() => console.log('DB Connection successful'));
 
 const app = require('./app');
 
