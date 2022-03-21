f259ffbe0739fca2c68a2d1762aa5ffa600f9d3b
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-03-21 16:18:30 -0300

    Creating a Schema and Model for our data in tours

--- server.js
+++ server.js
@@ -14,6 +14,25 @@ mongoose.connect(DB).then(() => console.log('DB Connection successful'));
 
 const app = require('./app');
 
+//creating a schema for our db documents
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+//creating a model out of the previous schema
+const Tour = mongoose.model('Tour', tourSchema);
+
 //establishing the port
 const port = process.env.PORT || 3000;
 
