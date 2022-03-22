a0f764785c1fcd2700d710b0ba75b6e871ea854a
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-03-22 12:40:20 -0300

    Importing our development data from our json to the database with scripts

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,49 @@
+//dependencies
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+//reading the env variables from our config.env file and saving them into Node environment variables
+dotenv.config({ path: './config.env' });
+
+//first, creating the complete mongodb connection string, with the password for the database
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+//connecting to MongoDB database
+mongoose.connect(DB).then(() => console.log('DB Connection successful'));
+
+//reading json file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+//importing data into database
+const importData = async () => {
+  try {
+    await Tour.create(tours); //it will create a new document for each of the objects in the tours array
+    console.log('Data successfully loaded!');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//deleting data from database
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany(); //it will remove all the documents from the database
+    console.log('Data successfully deleted');
+    process.exit();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
