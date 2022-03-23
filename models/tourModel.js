5aab9ff19dba796356d29842f3b3bf35c8d5a736
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-03-23 19:04:26 -0300

    Adding a Query Middleware (let's not show the secret tours)

--- models/tourModel.js
+++ models/tourModel.js
@@ -56,7 +56,11 @@ const tourSchema = new mongoose.Schema(
       default: Date.now(), //mongoose will convert it from miliseconds to date format
       select: false //createdAt will be hidden from the user
     },
-    startDates: [Date] //also will be parsed by mongoose
+    startDates: [Date], //also will be parsed by mongoose
+    secretTour: {
+      type: Boolean,
+      default: false
+    }
   },
   {
     toJSON: { virtuals: true }, //when data outputted as JSON: show virtual properties
@@ -69,7 +73,7 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7; //duration in days, divided by 7
 });
 
-//Document Middleware - Pre Middleware - associated to the save event
+//DOCUMENT MIDDLEWARE - Pre Middleware - associated to the save event
 //Runs before save() and create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true }); //this -> currently processed document, the one that is being saved to the db
@@ -81,6 +85,24 @@ tourSchema.pre('save', function(next) {
 //Runs after all pre middleware functions have completed
 tourSchema.post('save', function(doc, next) {
   console.log(doc);
+  next();
+});
+*/
+
+//QUERY MIDDLEWARE - Pre Middleware - associated to find() query
+//let's show only public tours, not secret ones
+tourSchema.pre(/^find/, function(next) {
+  this.find({ secretTour: { $ne: true } }); //this will point to the current query
+
+  this.start = Date.now();
+  next();
+});
+
+/*
+//QUERY MIDDLEWARE - Post Middleware - associated to find() query
+tourSchema.post(/^find/, function(docs, next) {
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  console.log(docs);
   next();
 });
 */
