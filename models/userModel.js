90df9886d2f81104ef7fd224985cdd87ebd01b21
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-03-29 11:13:32 -0300

    Password Management: Encrypting with bcrypt npm package and a pre-middleware

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 //dependencies
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 //creating a schema for our db documents
 const userSchema = new mongoose.Schema({
@@ -26,11 +27,34 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please, confirm your password'], //data validator
-    trim: true //removes all white spaces at the beginning and the end
+    required: [true, 'Please, confirm your password'], //data validator -> required as an input, not to be persisted in the db
+    trim: true, //removes all white spaces at the beginning and the end
+    validate: {
+      //this only works on create() and save()
+      validator: function(passConf) {
+        return passConf === this.password;
+      },
+      message: 'Passwords are not the same'
+    } //custom validator
   }
 });
 
+//encrypting passwords -> pre-middleware associated to "save" event
+userSchema.pre('save', async function(next) {
+  //Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //encrypting with bcryptjs
+  //hash -> async method from bcrypt (there is a sync version, don't use it here)
+  this.password = await bcrypt.hash(this.password, 12); //12 is a cost parameter (how CPU intensive the operation will be)
+
+  //remove the value from the confirmPassword field, we won't save it, just used for user's validation at the moment of creating/updating user
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+//creating a model out of the previous schema
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
