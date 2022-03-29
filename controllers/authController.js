5984eaef07b76a0362fb4b0d41ded7d77562ba18
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-03-29 12:12:49 -0300

    Solving a security flaw when creating a new user

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,12 @@ const catchAsync = require('./../utils/catchAsync');
 
 //sing up handler
 exports.signUp = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm
+  });
   //send the new user to the client
   res.status(201).json({
     status: 'success',
