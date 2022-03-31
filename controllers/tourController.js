cdc17299e91c2c3ab40607b20d046cb4aff98e56
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-03-31 13:07:55 -0300

    Filtering data to show in referenced child documents

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,7 +33,11 @@ exports.getAllTours = catchAsync(async (req, res, next) => {
   });
 });
 exports.getTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findById(req.params.id).populate('guides'); //populating child references to guide users in this tour
+  const tour = await Tour.findById(req.params.id).populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  }); //populating child references to guide users in this tour
+
   //Tour.findOne({_id: req.params.id})
 
   //HANDLING NOT FOUND RESOURCES
