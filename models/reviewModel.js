293b2571bac732cb8d529eb8cacbd2642bf03f90
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-03-31 13:46:32 -0300

    Creating references to tour and user in the review schema, also allowing virtual properties

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -2,18 +2,34 @@
 const mongoose = require('mongoose');
 
 //creating a schema for our db documents
-const reviewSchema = new mongoose.Schema({
-  review: {
-    type: String,
-    required: [true, 'Review cannot be empty!']
-  },
-  rating: { type: Number, min: 1, max: 5 },
-  createdAt: {
-    type: Date,
-    default: Date.now(), //mongoose will convert it from miliseconds to date format
-    select: false //createdAt will be hidden from the user
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review cannot be empty!']
+    },
+    rating: { type: Number, min: 1, max: 5 },
+    createdAt: {
+      type: Date,
+      default: Date.now(), //mongoose will convert it from miliseconds to date format
+      select: false //createdAt will be hidden from the user
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Review must belong to a tour']
+    }, //embedded document
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user']
+    }
+  }, //embedded document
+  {
+    toJSON: { virtuals: true }, //when data outputted as JSON: show virtual properties
+    toObject: { virtuals: true } //when data outputted as Object: show virtual properties
   }
-});
+);
 
 //creating a model out of the previous schema
 const Review = mongoose.model('Review', reviewSchema);
