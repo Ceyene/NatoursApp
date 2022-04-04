4664d4c97e5c718cfe9cc6a381748478c07161fa
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-04-04 15:44:36 -0300

    Creating card container and rendering all tours cards

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,20 @@
+//dependencies
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
 //rendering overview
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //1) Getting tour data from collection
+  const tours = await Tour.find();
+
+  //2) Building template
+
+  //3) Rendering that template using tour data from 1)
   res.status(200).render('overview', {
-    title: 'All tours'
+    title: 'All tours',
+    tours //tours: tours
   });
-};
+});
 //rendering tour
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
