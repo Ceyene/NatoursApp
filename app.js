01ba432ea0d98d2da3a60ac5af2bae09bb87c4b2
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-04-05 15:42:10 -0300

    solving Content Security Policy issues in helmet

--- app.js
+++ app.js
@@ -27,7 +27,30 @@ app.set('views', path.join(__dirname, 'views')); //using path module to create a
 app.use(express.static(path.join(__dirname, 'public'))); //using path module to create path to public folder
 
 //Setting secure HTTP Headers
-app.use(helmet());
+
+// Further HELMET configuration for Security Policy (CSP)
+const scriptSrcUrls = ['https://unpkg.com/', 'https://tile.openstreetmap.org'];
+const styleSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://fonts.googleapis.com/'
+];
+const connectSrcUrls = ['https://unpkg.com', 'https://tile.openstreetmap.org'];
+const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+      fontSrc: ["'self'", ...fontSrcUrls]
+    }
+  })
+);
 
 //Development logging
 if (process.env.NODE_ENV === 'development') {
