cb364fda438c39d3fbb17b4823b45de16a22838b
Cynthia Romero <cyn.jaz.romero@gmail.com>
2022-04-07 12:09:54 -0300

    Adding functionality to our password change form

--- public/js/index.js
+++ public/js/index.js
@@ -4,13 +4,14 @@
 import 'regenerator-runtime/runtime';
 import { displayMap } from './leaflet';
 import { login, logout } from './login';
-import { updateData } from './updateSettings';
+import { updateSettings } from './updateSettings';
 
 //DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
 if (mapBox) {
   //adding a map to each tour page
@@ -37,10 +38,38 @@ if (logOutBtn) {
 //adding functionality to our user data form
 if (userDataForm) {
   userDataForm.addEventListener('submit', e => {
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
     //preventing form from being sent before executing the rest of the code of this function
     e.preventDefault();
-    updateData(name, email);
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+  });
+}
+
+//adding functionality to our user password form
+if (userPasswordForm) {
+  userPasswordForm.addEventListener('submit', async e => {
+    //preventing form from being sent before executing the rest of the code of this function
+    e.preventDefault();
+
+    //changing button value -> updating message for user while API is working and encrypting password
+    document.querySelector('.btn--save-password').textContent = 'Updating...'; //
+
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    //waiting until the update is finished before continue
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    //once finished: changing button back to its original value
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+
+    //clearing inputs after sending form
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
 }
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -6,19 +6,22 @@ import { showAlert } from './alerts';
 
 //making a call to the API from the client side to update user data
 //update functionality with axios
-export const updateData = async (name, email) => {
+export const updateSettings = async (data, type) => {
   try {
+    //create url according to which type of data we are trying to update
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
+        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email
-      }
+      url,
+      data
     });
 
     if (res.data.status === 'success') {
-      showAlert('success', 'User data updated successfully');
+      showAlert('success', `User ${type} updated successfully`);
       window.setTimeout(() => {
         location.reload(true);
       }, 1000);
